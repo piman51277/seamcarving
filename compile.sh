382c2888d6a8ae84af1c9ee76e23ba5152832fa8
piman51277 <piman.dev@gmail.com>
2024-02-27 11:20:54 -0500

    cleanup

--- compile.sh
+++ compile.sh
@@ -5,10 +5,12 @@ export CURRENTDIR=$(pwd)
 # lib cpu
 g++ src/lib/cpu/*.cpp $CXXFLAGS -c -o bin/libcpu.o
 ar rcs bin/libcpu.a bin/libcpu.o
+rm bin/libcpu.o
 
 #lib gpu
 nvcc src/lib/gpu/*.cu $CUDAFLAGS -Xcompiler -fPIC -shared -dc -o bin/gpu.o
 ar rcs bin/libgpu.a bin/gpu.o
+rm bin/gpu.o
 
 # test cpu
 g++ src/main.cpp $CXXFLAGS -Lbin -lcpu -I/usr/include/SDL2 -D_REENTRANT -lSDL2 -L/usr/lib/x86_64-linux-gnu/ -lpng16 -lSDL2_image -o bin/main_cpu
@@ -16,3 +18,4 @@ g++ src/main.cpp $CXXFLAGS -Lbin -lcpu -I/usr/include/SDL2 -D_REENTRANT -lSDL2 -
 # test gpu
 g++ -c src/main.cpp $CXXFLAGS -I/usr/include/SDL2 -D_REENTRANT -o bin/main_gpu.o -Lbin -lgpu -L/usr/lib/x86_64-linux-gnu/ -lSDL2 -lpng16 -lSDL2_image
 nvcc bin/main_gpu.o $CUDAFLAGS -I/usr/include/SDL2 -D_REENTRANT -Lbin -lgpu -L/usr/lib/x86_64-linux-gnu/ -lSDL2 -lpng16 -lSDL2_image -o bin/main_gpu
+rm bin/main_gpu.o
